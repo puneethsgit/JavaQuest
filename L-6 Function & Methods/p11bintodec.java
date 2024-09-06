f1953dd3001a8541992a631e9474e1f57cb8c4d1
PUNEETH814 <princepuneeths814@gmail.com>
2024-09-06 22:39:45 +0530

    Binary added

---
+++ L-6 Function & Methods/p11bintodec.java
@@ -0,0 +1,19 @@
+public class p11bintodec {
+    public static void main(String[] args) {
+        binToDec(1011);
+    }
+
+    public static void binToDec(int binNum){
+        int myNum = binNum;
+        int decNum = 0;
+        int pow = 0;
+        while(binNum > 0){
+            int lastDigit = binNum % 10; //lastdigit retrieve
+            decNum = decNum + lastDigit * (int) Math.pow(2,pow);
+            pow++;
+            binNum = binNum / 10; //last digit remove
+        }
+        System.out.println("Decimail of " + myNum + " = " + decNum);
+    }
+
+}
