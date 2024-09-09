92a44492fcdf192f1d09dc07037c5d3492384b69
PUNEETH814 <princepuneeths814@gmail.com>
2024-09-10 01:20:50 +0530

    floyds pattern

--- L-7 Advance Pattern/P3InvertHalfPyramind.java
+++ L-7 Advance Pattern/P3InvertHalfPyramind.java
@@ -8,5 +8,16 @@
 
 
 public class P3InvertHalfPyramind {
-    
+    public static void pyramid(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i+1; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        pyramid(5);
+    }
 }
