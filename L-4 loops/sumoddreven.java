b18518e62b13d0ee1efd0f07bd41d1e3d8ecef40
PUNEETH814 <princepuneeths814@gmail.com>
2024-09-14 01:50:20 +0530

    added

---
+++ L-4 loops/sumoddreven.java
@@ -0,0 +1,32 @@
+//SUM OF ODD & EVEN NUM
+
+import java.util.Scanner;
+
+public class sumoddreven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number;
+        int choice;
+        int oddSum = 0;
+        int evenSum = 0;
+        do {
+            System.out.println("enter the number");
+            number = sc.nextInt();
+
+            if (number % 2 == 0) {
+                evenSum += number;
+            } else {
+                oddSum += number;
+            }
+
+            System.out.println("To continue to enter number press 1, To stop press 0 or other number");
+            choice = sc.nextInt();
+
+        } while (choice == 1);
+
+        System.out.println("Sum of even Numbers is " + evenSum);
+        System.out.println("Sum of odd Numbers is " + oddSum);
+        sc.close();
+    }
+}
