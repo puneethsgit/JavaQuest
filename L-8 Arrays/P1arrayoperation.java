5f7bdefc36a26f6e6a75bdad180dcd879aad767d
PUNEETH814 <princepuneeths814@gmail.com>
2024-09-15 22:23:33 +0530

    array started

---
+++ L-8 Arrays/P1arrayoperation.java
@@ -0,0 +1,8 @@
+public class P1arrayoperation {
+    public static void main(String[] args) {
+        //create array
+        int marks[] = new int[50];
+        int numbers[] = {1,2,3,4};
+        String fruits[] = {"apple", "mango", "orange"};
+    }
+}
