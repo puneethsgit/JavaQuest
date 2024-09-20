647e6452cab646823fbdf5ed85dd9192cfa93e0d
PUNEETH814 <princepuneeths814@gmail.com>
2024-09-21 00:13:15 +0530

    cmt

--- 00 - DSA/Q25trappingwater.java
+++ 00 - DSA/Q25trappingwater.java
@@ -1,6 +1,6 @@
 /* Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
  */
-public class P16trappingwater {
+public class Q25trappingwater {
     public static int trappingWater(int height[]){
         int n = height.length;
 
