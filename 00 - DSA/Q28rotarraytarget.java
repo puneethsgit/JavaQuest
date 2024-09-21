457a713d0782b32ae9d433d22ee4848a9ca95493
PUNEETH814 <princepuneeths814@gmail.com>
2024-09-22 02:35:56 +0530

    add

---
+++ 00 - DSA/Q28rotarraytarget.java
@@ -0,0 +1,48 @@
+/* There is an integer array nums sorted in ascending order (with distinct values).
+
+Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ */
+public class Q28rotarraytarget {
+    
+    public static void main(String[] args) {
+        int nums[] = {4,5,6,7,0,1,2};
+        int target = 4;
+        System.out.println(search(nums,target));   
+    }
+
+    public static int search(int[] nums, int target) {
+        int n = nums.length;
+        int low = 0, high = n-1;
+        while(low<=high){
+            int mid = (low+high) / 2;
+            
+            //if mid points to the target
+            if(nums[mid] == target){
+                return mid;
+            }
+
+            //if left part is sorted
+            if(nums[low] <= nums[mid]){
+                if(nums[low] <= target && target <= nums[mid]){
+                    //element exist
+                    high = mid - 1;
+                }else{
+                    //element does not exist
+                    low = mid + 1;
+                }
+            }else{  //if right part is sorted
+                if(nums[mid] <= target && target <= nums[high]){
+                    low = mid + 1;
+                }else{
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;  // elements does exist in array
+    }
+}
