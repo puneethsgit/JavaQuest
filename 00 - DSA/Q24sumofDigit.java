5c266e93e1e1bb33824c425f2485687676171664
PUNEETH814 <princepuneeths814@gmail.com>
2024-09-25 01:52:57 +0530

    2 pointer approach

--- 00 - DSA/Q24sumofDigit.java
+++ 00 - DSA/Q24sumofDigit.java
@@ -1,3 +1,7 @@
+/* You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+Return the maximum amount of water a container can store.
+Notice that you may not slant the container. */
 import java.util.Scanner;
 
 public class Q24sumofDigit {
@@ -10,6 +14,7 @@ public class Q24sumofDigit {
         }
         return sum;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the Numbers: ");
---
+++ 00 - DSA/Q31containerwater.java
@@ -0,0 +1,41 @@
+public class Q31containerwater {
+     //Brute force Approach
+     public static int maxArea(int[] height) {
+        int maxWater = 0;
+        int n = height.length;
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                int w = j-i;
+                int ht = Math.min(height[i], height[j]);
+                int currWater = w * ht;
+                maxWater = Math.max(currWater, maxWater);
+            }
+        }
+        return maxWater;
+    }   
+
+    //2 pointer approach
+    public static int maxArea2(int[] height) {
+        int maxWater = 0;
+        int n = height.length;
+        int lp = 0; //leftpointer
+        int rp = n-1; //rightpointer
+        while(lp<rp){
+            int w = rp-lp; //width
+            int ht = Math.min(height[lp],height[rp]); //min height
+            int currentWater = w * ht;
+            maxWater = Math.max(currentWater, maxWater);
+           if(height[lp] < height[rp]){
+                lp++;
+           }else{
+                rp--;
+           }
+        }
+        return maxWater;
+    }
+
+    public static void main(String[] args) {
+        int height[] = {1,8,6,2,5,4,8,3,7};
+        System.out.println(maxArea(height));
+    }
+}
