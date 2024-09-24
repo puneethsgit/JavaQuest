9a3b90e55dd2504bda40173c3754118782654c41
PUNEETH814 <princepuneeths814@gmail.com>
2024-09-24 20:38:04 +0530

    product of self

--- L-8 Arrays/Q6productofarr.java
+++ L-8 Arrays/Q6productofarr.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 
 public class Q6productofarr {
 
-    //Brute Force Approach
+    //Brute Force Approach TC:O(n2)
     public static int[] productExpectSelf1(int nums[]){
         int n = nums.length;
         int answer[] = new int[n];
@@ -20,10 +20,52 @@ public class Q6productofarr {
         }
         return answer;
     }
+    //prefix suffix TC-O(3n) SC-O(n)
+    public static int[] productExceptSelf2(int nums[]){
+        int n = nums.length;
+        //prefix
+        int prefix[] = new int[n];
+        prefix[0] = 1;
+        for(int i=1; i<n; i++){
+            prefix[i] = prefix[i-1] * nums[i-1];
+        }
+        //suffix
+        int suffix[] =  new int [n];
+        suffix[n-1] = 1; 
+        for(int i=n-2; i>=0; i-- ){
+            suffix[i] = suffix[i+1] * nums[i+1];
+        }
+        //answer
+        int answer[] = new int[n];
+        for(int i=0; i<n; i++){
+            answer[i] = prefix[i] * suffix[i];
+        }
+     return answer;   
+    }
+
+    //prefix suffix TC-O(3n) SC-O(1)
+    public static int[] productExceptSelf(int nums[]){
+        int n = nums.length;
+         int answer[] = new int[n];
+         Arrays.fill(answer,1);
+
+        //prefix directly stored in answer
+        for(int i=1; i<n; i++){
+            answer[i] = answer[i-1] * nums[i-1];
+        }
+
+        //suffix
+        int suffix=1;
+        for(int i=n-2; i>=0; i-- ){
+           suffix *= nums[i+1];
+           answer[i] *= suffix;
+        }
+     return answer;   
+    }
 
     public static void main(String[] args) {
         int nums[] = {1,2,3,4};
-        int answer[] = productExpectSelf1(nums);
+        int answer[] = productExceptSelf(nums);
         // for(int i=0; i<answer.length;i++){
         //     System.out.print(answer[i] + " ");
         // }
