de49d3b2f0620aa0f010879e7776d5edb2f65efa
PUNEETH814 <princepuneeths814@gmail.com>
2024-09-26 21:56:42 +0530

    2d array

---
+++ L-10 2D Arrays/Q1creation2d.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class Q1creation2d {
+
+    //search key
+    public static boolean searchKey(int matrix[][], int key){
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                if(key == matrix[i][j]){
+                    System.out.println("key Found at cell (" + i + "," + j + ")");
+                    return true;
+                }
+            }
+        }
+        System.out.println("Not found");
+        return false;
+    }
+
+    //max & min
+    public static void maxminElement(int matrix[][]){
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                if(matrix[i][j] > max){
+                    max = matrix[i][j];
+                }
+                if(matrix[i][j] < min){
+                    min = matrix[i][j];
+                }
+            }
+        }
+        System.out.println("maximum : " + max);
+        System.out.println("minimum : " + min);
+
+    }
+    public static void main(String[] args) {
+        int matrix[][] = new int[3][3];
+        int n=matrix.length, m=matrix[0].length;
+
+        //input
+        Scanner sc = new Scanner(System.in);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        //output
+        sc.close();
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        //searchkey
+        searchKey(matrix, 6);
+
+        //maxmin
+        maxminElement(matrix);
+    }
+}
---
+++ L-10 2D Arrays/Q2sprialmatrix.java
@@ -0,0 +1,51 @@
+public class Q2sprialmatrix {
+
+    public static void printSprial(int matrix[][]){
+        int startRow=0;
+        int startCol = 0;
+        int endRow = matrix.length-1;
+        int endCol = matrix[1].length-1;
+
+
+        while(startRow <= endRow && startCol <= endCol){  
+            //while we can also use while(true) i = row and j = col
+
+            //top
+            for(int j=startRow; j<=endCol; j++){
+                System.out.print(matrix[startRow][j]+" ");
+            }
+
+            //right
+            for(int i=startRow+1; i<=endRow; i++){
+                System.out.print(matrix[i][endCol]+" ");
+            }
+
+            //bottom
+            for(int j=endCol-1; j>=startCol; j--){
+                if(startRow == endRow){
+                    break;
+                }
+                System.out.print(matrix[endRow][j]+" ");
+            }
+
+            //left
+            for(int i=endRow-1; i>=startRow+1; i--){
+                if(startCol == endCol){
+                    break;
+                }
+                System.out.print(matrix[i][startCol]+" ");
+            }
+
+           
+            startCol++;
+            startRow++;
+            endCol--;
+            endRow--;
+        }
+        System.out.println(); //next line
+    }
+    public static void main(String[] args) {
+        int matrix[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+        printSprial(matrix);
+    }
+}
