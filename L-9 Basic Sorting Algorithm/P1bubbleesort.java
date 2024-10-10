1a8e82e7f8fa26193ee3ce49ee6a250a5fade3f5
PUNEETH814 <princepuneeths814@gmail.com>
2024-10-10 21:54:01 +0530

    bubble sort

--- L-9 Basic Sorting Algorithm/P1bubbleesort.java
+++ L-9 Basic Sorting Algorithm/P1bubbleesort.java
@@ -4,7 +4,7 @@ public class P1bubbleesort {
     public static void bubblesortt(int arr[]) {
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - 1 - i; j++) {
-                if (arr[j] < arr[j + 1]) {
+                if (arr[j] > arr[j + 1]) {
                     // swap
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
