181f65cb0cc3ac61c87d943a69467f183fc2968c
PUNEETH814 <princepuneeths814@gmail.com>
2024-11-17 19:32:20 +0530

    add

--- L-14 Recursion/P1recursion.java
+++ L-14 Recursion/P1recursion.java
@@ -1,5 +1,24 @@
+/* Print Number in decreasing order n to 1  */
 public class P1reccursion {
     public static void main(String[] args) {
-        System.out.println("HELLO");
+        recursion(10);
+    }
+
+    public static void recursion(int num) {
+        if (num < 1) {
+            return;
+        }
+        System.out.print(num + " ");
+        recursion(num -1);
+    }
+
+    public static void printdec(int n) {
+        //BaseCase
+        if(n == 1) {
+            System.out.println(n);
+            return;
+        }
+        System.out.println(n);
+        printdec(n-1); //recursive Call
     }
 }
