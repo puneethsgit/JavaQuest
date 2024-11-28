449794cb9111104d04afc4df2e7d9a0292b6c896
PUNEETH814 <princepuneeths814@gmail.com>
2024-11-28 18:39:22 +0530

    recursion

--- L-14 Recursion/P14binarystring.java
+++ L-14 Recursion/P14binarystring.java
@@ -1,27 +1,12 @@
-/* Given a positive integer number N. The task is to generate all the binary strings of N bits. These binary strings should be in ascending order.
-Examples: 
-
-Input: 2
-Output:
-0 0
-0 1
-1 0
-1 1 */
+/*  The task is to generate all the binary strings of N bits. dont include consecutive ones */
 public class P14binarystring {
     
     public static void main(String[] args) {
-        printBinString(3, 0, "");
+        binString1(3, 1, ""); //no consecutive 1S
+        System.out.println("--------");
+        printBinString(3, 0, ""); //no consecutive 0S
     }
     public static void printBinString(int n, int lastPlace, String str) {
-
-        //work
-        // if(lastPlace == 0) {
-        //     printBinString1(n-1, 0, str.append("0"));
-        //     printBinString1(n-1, 1, str.append("1"));
-        // } else {
-        //     printBinString1(n-1, 0, str.append("0"));
-        // }
-
         if(n==0) {
             System.out.println(str);
             return;
@@ -29,6 +14,16 @@ public class P14binarystring {
         printBinString(n-1, 0, str+"0");
         if(lastPlace == 0) {
             printBinString(n-1, 1, str+"1");
+        }
+    }
+    public static void binString1(int n, int lastPlace, String str) {
+        if(n == 0) {
+            System.out.println(str);
+            return;
+        }
+        binString1(n-1, 1, str+"1");
+        if(lastPlace == 1) {
+            binString1(n-1, 0, str+"0");
         }
     }
 }
