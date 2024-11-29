efe97bb165be4979ebf7370aeaa41642b2ff523e
PUNEETH814 <princepuneeths814@gmail.com>
2024-11-29 17:06:19 +0530

    towerofhanoi

--- L-14 Recursion/Q5towerofhanoi.java
+++ L-14 Recursion/Q5towerofhanoi.java
@@ -9,6 +9,18 @@ You have the following constraints:
 last using Stacks.
  */
 public class Q5towerofhanoi {
-
+    public static void towerOfHanoi(int n, String src, String helper, String dest) {
+        if(n == 1) {
+            System.out.println("transfer disk " + n + " from " + src+ " to "+ dest);
+            return;
+        }
+        towerOfHanoi(n-1, src, dest, helper);
+        System.out.println("transfer disk " + n + " from " + src+ " to "+ dest);
+        towerOfHanoi(n-1, helper, src, dest);
+    }
+    public static void main(String[] args) {
+        int n = 3;
+        towerOfHanoi(n, "S", "H", "D");
+    }
     
 }
