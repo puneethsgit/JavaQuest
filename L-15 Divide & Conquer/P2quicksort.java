02f553439301d750786b1628a466c76ef730d3fd
PUNEETH814 <princepuneeths814@gmail.com>
2024-12-01 15:46:39 +0530

    mergesort

--- L-15 Divide & Conquer/P2quicksort.java
+++ L-15 Divide & Conquer/P2quicksort.java
@@ -7,10 +7,38 @@ public class P2quicksort {
     }
 
     public static void quickSort(int arr[], int si, int ei) {
+        if(si >= ei) {
+            return;
+        }
 
+        int pIdx = partition(arr, si, ei);  //sort L & R and also place pivot to right position
+        quickSort(arr, si, pIdx-1); //Left
+        quickSort(arr, pIdx+1, ei); //right
+    }
+
+    public static int partition(int arr[], int si, int ei) {
+        int pivot = arr[ei];
+        int i = si-1; //to make place for element smaller than pivot
+
+        for(int j=si; j<ei; j++) {
+            if(arr[j] <= pivot) {
+                i++;
+                //swap
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        //to place pivot on right position
+        i++;
+        int temp = pivot;
+        arr[ei] = arr[i];
+        arr[i] = temp;
+        return i; //pivot index
     }
     public static void main(String[] args) {
-        int arr[] = {6,3,9,8,2,5};
+        int arr[] = {6,3,9};
         quickSort(arr, 0, arr.length-1);
+        printArr(arr);
     }
 }
