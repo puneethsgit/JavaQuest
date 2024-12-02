fd49940402592b4e0d6a7689657e162af84f61a6
PUNEETH814 <princepuneeths814@gmail.com>
2024-12-02 20:55:54 +0530

    inversioncount

--- L-15 Divide & Conquer/Q3inversioncount.java
+++ L-15 Divide & Conquer/Q3inversioncount.java
@@ -1,4 +1,8 @@
-/*  */
+/* Given an array of integers Find the inversion count in the array
+ INVERSIONCOUNT : For an array, inversion count indicates how far (or close) the array is from being sorted. if the array is already sorted then the inversion count is zero (0). if an array is sorted in the reverse order then inversion count is maximum formally for two elements a[i] and a[j] form an inversion if a[i] > a[j] and i<j
+SAMPLE INPUT : N=5, arr[] = {2,4,1,3,5}
+SAMPLE OUTPUT : 3 because it has 3 inversion = (2,1) (4,1) (4,3)
+ */
 public class Q3inversioncount {
     
 }
