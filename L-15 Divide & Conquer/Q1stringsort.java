3ab4fde63cfb1839cc1bcee3ff33ddf38a66cbea
PUNEETH814 <princepuneeths814@gmail.com>
2024-12-02 14:36:40 +0530

    stringsort

--- L-15 Divide & Conquer/Q1stringsort.java
+++ L-15 Divide & Conquer/Q1stringsort.java
@@ -21,20 +21,20 @@ public class Q1stringsort {
     static String[] merge(String[] arr1, String[] arr2) {
         int m = arr1.length;
         int n = arr2.length;
-        String[] arr3 = new String[m + n]; //temp
+        String[] arr3 = new String[m + n]; 
 
-        int idx = 0;
+        int idx = 0;//pointer for arr3
 
-        int i = 0;
-        int j = 0;
+        int i = 0;//pointer for arr1
+        int j = 0;//pointer for arr2
 
         while (i < m && j < n) {
-            if(isAlphabeticallySmaller(arr1[i],arr2[j])) {
+            if(isAlphabeticallySmaller(arr1[i],arr2[j])) {  //true - arr1 element added
                 arr3[idx] = arr1[i];
                 i++;
                 idx++;
             }
-            else {
+            else { //false - arr2 element added
                 arr3[idx] = arr2[j];
                 j++;
                 idx++;
@@ -58,8 +58,8 @@ public class Q1stringsort {
     }
 
     //return true if str1 appears before str2 in alphabetical order
-    static boolean isAlphabeticallySmaller(String str1, String str2) {
-        if(str1.compareTo(str2) < 0) {
+    static boolean isAlphabeticallySmaller(String str1, String str2) { //lexicographical comparison
+        if(str1.compareTo(str2) < 0) { // 0 : equal , below -1 : str1 smaller than str2 , 1 str1 greater than str
             return true;
         }
         return false;
