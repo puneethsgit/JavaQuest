982a0f175622065204a2a466f1c8798b078098d0
PUNEETH814 <princepuneeths814@gmail.com>
2024-12-25 15:16:25 +0530

    monotonic

---
+++ L-18 Arraylist/Q1Monotonic.java
@@ -0,0 +1,30 @@
+/* An Arraylist is monotonic if it is either monotone increasing or monotone decreasing.
+An Arraylist nums is monotone increasing if for all i <= j, nums.get(i) <= nums.get(j). An
+Arraylist nums is monotone decreasing if for all i <= j, nums.get(i) >= nums.get(j).
+Given an integer Arraylist nums, return true if the given list is monotonic, or false otherwise */
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Q1Monotonic {
+
+    public static boolean monotonic(ArrayList<Integer> list) {
+        boolean inc = true;
+        boolean dec = true;
+
+        for(int i=0; i<list.size()-1; i++) {
+            if(list.get(i) > list.get(i+1)) { //Monotonic Increasing Check:
+                inc = false;
+            }
+            if(list.get(i) < list.get(i+1)) {  // Monotonic Decreasing Check:
+                dec = false;
+            }
+        }
+        return inc || dec;
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        Collections.addAll(list, 1,2,2,3);
+        System.out.println(monotonic(list));
+    }
+}
