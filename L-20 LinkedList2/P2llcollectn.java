ee9808c1676b1b5b32b00be0a3e9dc3986b621a1
PUNEETH814 <princepuneeths814@gmail.com>
2025-01-02 20:58:12 +0530

    linkedlist collection

--- L-20 LinkedList2/P2llcollectn.java
+++ L-20 LinkedList2/P2llcollectn.java
@@ -1,3 +1,28 @@
+import java.util.LinkedList;
 public class P2llcollectn {
-    
+    public static void main(String[] args) {
+        //create 
+        LinkedList<Integer> ll = new LinkedList<>();
+        
+        //add
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addFirst(0);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addLast(5);
+        //0->1->2->3->4->5
+
+        //print 
+        System.out.println(ll);
+        
+        //remove
+        ll.removeLast();
+        ll.removeFirst();
+        System.out.println(ll);
+
+        //print head tail
+        System.out.println(ll.getFirst());
+        System.out.println(ll.getLast());
+    }
 }
