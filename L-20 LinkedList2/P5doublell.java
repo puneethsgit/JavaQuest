5e739d26ea78dc1f24a037988ed61e8063213a60
PUNEETH814 <princepuneeths814@gmail.com>
2025-01-03 17:46:51 +0530

    double  linked list

--- L-20 LinkedList2/P5doublell.java
+++ L-20 LinkedList2/P5doublell.java
@@ -16,8 +16,78 @@ public class P5doublell {
     public static Node tail;
     public static int size;
 
+    //addfirst
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if(head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
+
+    //print
+    public void print() {
+        Node temp = head;
+        //System.out.print("null" + " <-> ");
+        while(temp != null) {
+            System.out.print(temp.data + " <-> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    //removeFirst
+    public int removeFirst() {
+        if(head == null) {
+            System.out.println("DLL is empty");
+            return Integer.MIN_VALUE; //negative number return
+        }
+        if(size == 1) {  //corner case
+            int val = head.data;
+            head = tail = null;
+            size--;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        head.prev = null; //if node is single will get error here so corner case added
+        size--;
+        return val;
+    }
+
+    //add last
+    //remove last
+
+    //reverse
+    public void reverse() {
+        Node curr = head;
+        Node prev = null;
+        Node next;
+        while(curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            curr.prev = next;
+            prev = curr;
+            curr = next;
+        }
+        head = prev; //head becomes prev because curr will be now null
+    }
     public static void main(String[] args) {
         P5doublell dll = new P5doublell();
-            
+        dll.addFirst(3);
+        dll.addFirst(2);
+        dll.addFirst(1);
+        dll.print();
+        System.out.println(dll.size);
+        //dll.removeFirst();
+        //System.out.println(dll.size);
+        //dll.print();
+        dll.reverse();
+        dll.print();
     }
 }
